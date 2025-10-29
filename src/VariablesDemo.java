3b8de47d9cf8f455e51de331da6f241d40017dbc
Arshdeep_Kaur <arshdeep_kaur@Arshdeep-Kaurs-MacBook-Air.local>
2025-10-30 01:30:23 +0530

    Day 1: Basics of Java

---
+++ src/VariablesDemo.java
@@ -0,0 +1,15 @@
+public class VariablesDemo {
+    public static void main(String[] args){
+        int age=25;
+        double salary=85.00;
+        char grade='A';
+        boolean isEmployed=true;
+        String name="Arsh";
+
+        System.out.println(name);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(age);
+        System.out.println(isEmployed);
+    }
+}
